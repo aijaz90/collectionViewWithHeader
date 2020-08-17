1be4a6e918d08e884fa0d3d82573acf435956271
aijaz ali <aijazmike90@gmail.com>
2020-08-17 18:22:42 +0500

    Colelction with Resuseable

---
+++ CollectionViewWithHeader/HomeCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionReusableView.swift
+//  CollectionViewWithHeader
+//
+//  Created by Aijaz Ali on 17/08/2020.
+//  Copyright © 2020 IT Majlis2019. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionReusableView: UICollectionReusableView {
+        
+}
---
+++ CollectionViewWithHeader/cell/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewWithHeader
+//
+//  Created by Aijaz Ali on 17/08/2020.
+//  Copyright © 2020 IT Majlis2019. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
