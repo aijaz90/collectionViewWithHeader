b6ff7c3dca3a7c6c8436ed4a95201c01ee280094
it.iosdev2 <it.iosdev2@dawateislami.net>
2020-08-17 12:32:41 +0500

    Initial Commit

---
+++ CollectionViewWithHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewWithHeader
+//
+//  Created by Aijaz Ali on 17/08/2020.
+//  Copyright © 2020 IT Majlis2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
